db33ce4fa46445d8d2172558e15fc9bf29df6804
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-16 10:28:52 -0300

    endpoint passando um id

--- src/main/kotlin/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/mercadolivro/controller/CustomerController.kt
@@ -9,15 +9,28 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("customer")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Danilo", "meuemail@email.com")
+    fun getAll(): List<CustomerModel> {
+        return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) //Especifica que o recurso foi criado com sucesso
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+        val id = if (customers.isEmpty()) {
+            1
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id}.first() //Ao encontrar para no primeiro registro o ID é unico
+    }
+
 
 }
