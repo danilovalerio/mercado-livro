e29f82bed7068674931f34a1fc0ec5a2feb5bf32
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-16 18:06:14 -0300

    Convertendo a MODEL em uma ENTIDADE

--- src/main/kotlin/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/mercadolivro/model/CustomerModel.kt
@@ -1,7 +1,14 @@
 package mercadolivro.model
 
+import javax.persistence.*
+
+@Entity(name = "customer")
 data class CustomerModel(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int? = null,
+    @Column
     var name: String,
+    @Column
     var email: String
 )
