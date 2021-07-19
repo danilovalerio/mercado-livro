d040c2afb87b2635a60629f545deac55785d74ab
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-19 18:49:40 -0300

    service annotation para spring

--- src/main/kotlin/mercadolivro/service/BookService.kt
+++ src/main/kotlin/mercadolivro/service/BookService.kt
@@ -2,7 +2,9 @@ package mercadolivro.service
 
 import mercadolivro.model.BookModel
 import mercadolivro.repository.BookRepository
+import org.springframework.stereotype.Service
 
+@Service
 class BookService(
     val bookRepository: BookRepository
 ) {
