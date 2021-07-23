2f8c661b9c5bdbe708178114df396a6fdfaa6a80
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-23 18:36:32 -0300

    fix dependencia Swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	implementation("io.springfox:springfo-swagger2:3.0.0")
-	implementation("io.springfox:springfo-swagger-ui:3.0.0")
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
 
 	implementation( "org.flywaydb:flyway-core:7.11.2")
 
--- src/main/kotlin/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/mercadolivro/config/SwaggerConfig.kt
@@ -1,5 +1,8 @@
 package mercadolivro.config
 
+import org.springframework.context.annotation.Configuration
 
+@Configuration
+@EnableSwagger2
 class SwaggerConfig {
 }
